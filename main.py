77ffed918d6dd2cc277bb615a5cf748053dfb7e0
Arthur <arthurlm1234@gmail.com>
2023-12-04 07:44:40 -0300

    argparse

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import argparse
 from criar_grafo import carregar_problema_tsp, carregar_solucao_optima, calcular_peso_solucao_optima
 from metricas import executarAlgoritmo, salvar_em_csv
 from twice_around_the_tree import twiceAroundTheTreeTSP
 from christofides import christofidesTSP
 from branch_and_bound import branchAndBoundTSP
 
-nome_dataset = 'berlin52'
+# Configuração do argparse para aceitar o nome do dataset via linha de comando
+parser = argparse.ArgumentParser(description='Executar algoritmos TSP em um dataset específico.')
+parser.add_argument('dataset', help='Nome do dataset (sem a extensão do arquivo)')
+args = parser.parse_args()
+
+nome_dataset = args.dataset
 nome_arquivo_csv = 'resultados_tsp.csv'
 
-# Supondo que as funções carregar_problema_tsp, carregar_solucao_optima e calcular_peso_solucao_optima estejam definidas
+# Carregar dados do dataset especificado
 grafo = carregar_problema_tsp(f'datasets/{nome_dataset}.tsp')
 tour_otima = carregar_solucao_optima(f'datasets/{nome_dataset}.opt.tour')
 peso_otimo = calcular_peso_solucao_optima(grafo, tour_otima)
